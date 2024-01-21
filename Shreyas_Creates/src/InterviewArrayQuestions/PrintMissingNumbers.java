b97fc0866e5700e2f32a4fdbe1204421a5076b62
shrgour <shrgour@cisco.com>
2024-01-21 16:13:57 +0530

    Swap Strings w/o 3rd variable and Print Missing Numbers

--- Shreyas_Creates/src/InterviewArrayQuestions/PrintMissingNumbers.java
+++ Shreyas_Creates/src/InterviewArrayQuestions/PrintMissingNumbers.java
@@ -4,13 +4,16 @@ public class PrintMissingNumbers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] array1 = {1,2,4,6,8,10,12};
+		int[] array1 = {1,2,4,8,10,12};
 		
-//		Question asked
-//		Print missing numbers from above array as in
-//		Print 3,5,6,7,9,11
+		
+		/*
+		Question asked
+		Print missing numbers from above array as in
+	    Print 3,5,6,7,9,11
+        */
+ 
 		int counter = 1;
-		//int i=0;
 		for(int i = 0; i< array1.length ; i++) {
 			if(array1[i] == counter)
 			{
---
+++ Shreyas_Creates/src/InterviewQuestionsStrings/SwapStrings.java
@@ -0,0 +1,20 @@
+package InterviewQuestionsStrings;
+
+public class SwapStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			String first = "Shreyas";
+			String second = "Gour";
+			
+			first =  first + second;
+			System.out.println(first);
+			
+			second = first.substring(0,first.length()-second.length());
+			first = first.substring(second.length());
+			
+			System.out.println(first);
+			System.out.println(second);
+	}
+
+}
